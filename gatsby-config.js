edd6244c5b2ccd160eacafe2352e39528702506e
michael elizondo <michaelf.elizondo@gmail.com>
2021-06-24 12:54:13 -0500

    updated title

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Michael Elizondo`,
+      summary: `I have red hair.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
